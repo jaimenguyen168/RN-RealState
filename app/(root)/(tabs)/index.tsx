b41628412400d5d2e99f7368ea651bdde940949a
Jaime <nguyenphuocdat168@gmail.com>
2024-12-21 00:40:51 -0500

    add cards

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import {SafeAreaView} from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/Search";
+import {Card, FeaturedCard} from "@/components/Cards";
 
 export default function Index() {
   return (
@@ -42,6 +43,30 @@ export default function Index() {
                 See All
               </Text>
             </TouchableOpacity>
+          </View>
+
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Our Recommendations
+            </Text>
+
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="flex flex-row gap-5 mt-5">
+            <Card />
+            <Card />
           </View>
         </View>
       </View>
