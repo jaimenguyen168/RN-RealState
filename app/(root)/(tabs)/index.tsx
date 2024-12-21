224c3cf64408e6b04cc741bf45cc73a65fd8404f
Jaime <nguyenphuocdat168@gmail.com>
2024-12-21 00:07:56 -0500

    add search and debounded funcitonality

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,50 @@
-import { Text, View } from "react-native";
+import {Image, Text, TouchableOpacity, View} from "react-native";
 import {Link} from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+import Search from "@/components/Search";
 
 export default function Index() {
   return (
-    <View className="h-full flex items-center justify-center">
-      <Text className="text-2xl font-rubik my-5">Welcome to Real Estate</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image
+              source={images.avatar}
+              className="size-12 rounded-full"
+            />
+
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Jaime Nguyen
+              </Text>
+            </View>
+          </View>
+
+         <Image source={icons.bell} className="size-6"/>
+        </View>
+
+        <Search />
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
